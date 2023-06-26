477a3bc20876688a7a8278e73cf3ae54ed9dc49c
My Duy Thanh <thanhmd@vnpt-technology.vn>
2023-06-26 11:32:02 +0700

    test4

--- main/src/main/java/test.java
+++ main/src/main/java/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
     }
 }
